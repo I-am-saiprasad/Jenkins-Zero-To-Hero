df2e8bdb07412ec8d2d54c0348d5c772a43172f9
Sai Prasad <sai@Sais-MacBook-Air.local>
2025-01-28 22:52:28 +0530

    added pom.xml for mnv clean install

--- helloworld-app/my-helloworld.java
+++ helloworld-app/my-helloworld.java
@@ -1,7 +1,5 @@
-// HelloWorld.java
 public class HelloWorld {
     public static void main(String[] args) {
-        // Print "Hello, World!" to the console
         System.out.println("Hello, World!");
     }
 }
